b7121cc3e369acfcaab856350cb37cf44bb5ed3c
rodion <rodionaaafgh@gmail.com>
2018-12-05 18:57:51 +0300

    Updates

---
+++ precache-manifest.cfc19176f576263cef27d07d8e534faf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0985ec1c57ac6c34f74b",
+    "url": "/increaser-timeline/static/js/main.0985ec1c.chunk.js"
+  },
+  {
+    "revision": "4b89dba362183df50feb",
+    "url": "/increaser-timeline/static/js/1.4b89dba3.chunk.js"
+  },
+  {
+    "revision": "b52bc3727334167dfbde",
+    "url": "/increaser-timeline/static/js/runtime~main.b52bc372.js"
+  },
+  {
+    "revision": "8b790770d78670481e71b25819256a3f",
+    "url": "/increaser-timeline/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/increaser-timeline/precache-manifest.cfc19176f576263cef27d07d8e534faf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/increaser-timeline/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
